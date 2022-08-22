e3ae2ba9bf26412a3a84840df19dbf6ae79ecac8
Nixon Nelson <nixonnelson22@gmail.com>
2022-08-22 18:47:39 +0530

    styling added

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+//drawing canvas for captcha
+const canvas = document.getElementById("captcha");
+const ctx= canvas.getContext("2d");
+let y=canvas.height;
+let x=canvas.width;
+ctx.font();
+ctx.fillText("hello",0,0);
