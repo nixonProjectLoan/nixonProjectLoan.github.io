cbac399ea82c44ce0e80af6855e482a4a29a87c2
Nixon Nelson <nixonnelson22@gmail.com>
2022-08-23 00:01:17 +0530

    basic input checks and captcha is almost ready

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,21 @@
 let r1 = Math.floor((Math.random()*100)+1);
 let r2 = Math.floor((Math.random()*100)+1);
-
-
-
-function captcha(r1,r2,operator="*"){
-let text=""+r1+operator+r2;
-//drawing canvas for captcha
-const canvas = document.getElementById("mycaptcha");
-const ctx = canvas.getContext('2d');
-ctx.font='100px serif';
-// ctx.textAlign='center'; 
-ctx.fillText(text,100,100);
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let pan = document.getElementById("pan").value;
+let lamt = document.getElementById("lamt").value;
+function captchacreate(r1,r2,operator="*"){
+    let text=""+r1+operator+r2;
+    //drawing canvas for captcha
+    const canvas = document.getElementById("mycaptcha");
+    const ctx = canvas.getContext('2d');
+    ctx.font='100px serif';
+    // ctx.textAlign='center'; 
+    ctx.fillText(text,100,100);
 }
 
-
-captcha(r1,r2);
-document.getElementById("cpt").onclick=captchatest;
 function captchatest(){
-
-    const user_ans = document.getElementById("cpt");
+    const user_ans = document.getElementById("cpt").value;
     let result=r1*r2;
     if(user_ans == result)
         console.log("True");
@@ -26,3 +23,9 @@ function captchatest(){
         console.log("False");
     
 }
+
+
+captchacreate(r1,r2);//genertes captcha
+document.getElementById("cpt").onclick = captchatest;
+
+
